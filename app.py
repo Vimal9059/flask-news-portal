bfae70ec55575a8b943529b30f470ddaf5a6b718
Vimal <sam_vim@yahoo.com>
2025-07-30 15:29:27 +0000

    singapore added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, requests
 import feedparser
 
 app = Flask(__name__)
@@ -7,7 +7,8 @@ RSS_FEEDS = {
     'us': 'https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en',
     'in': 'https://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN:en',
     'ca': 'https://news.google.com/rss?hl=en-CA&gl=CA&ceid=CA:en',
-    'gb': 'https://news.google.com/rss?hl=en-GB&gl=GB&ceid=GB:en'
+    'gb': 'https://news.google.com/rss?hl=en-GB&gl=GB&ceid=GB:en',
+    'sg': 'https://news.google.com/rss?hl=en-SG&gl=SG&ceid=SG:en'
 }
 
 @app.route('/')
